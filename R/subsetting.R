4b24beef5913dd0d0b986503b74ed24e075f574a
mathesong <mathesong@gmail.com>
2022-10-03 11:38:41 +0200

    Display fitted parameters in the report, and clean up the config files

--- R/subsetting.R
+++ R/subsetting.R
@@ -12,15 +12,15 @@ parse_config_subsets <- function(config) {
   InstitutionName <- stringr::str_remove_all(config$Subsets$InstitutionName, " ")
   PharmaceuticalName <- stringr::str_remove_all(config$Subsets$PharmaceuticalName, " ")
 
-  sub <- str_split(config$Subsets$sub, ";")[[1]]
-  ses <- str_split(config$Subsets$ses, ";")[[1]]
-  rec <- str_split(config$Subsets$rec, ";")[[1]]
-  task <- str_split(config$Subsets$task, ";")[[1]]
-  run <- str_split(config$Subsets$run, ";")[[1]]
-  TracerName <- str_split(config$Subsets$TracerName, ";")[[1]]
-  ModeOfAdministration <- str_split(config$Subsets$ModeOfAdministration, ";")[[1]]
-  InstitutionName <- str_split(config$Subsets$InstitutionName, ";")[[1]]
-  PharmaceuticalName <- str_split(config$Subsets$PharmaceuticalName, ";")[[1]]
+  sub <-  str_split(sub, ";")[[1]]
+  ses <-  str_split(ses, ";")[[1]]
+  rec <-  str_split(rec, ";")[[1]]
+  task <- str_split(task, ";")[[1]]
+  run <-  str_split(run, ";")[[1]]
+  TracerName <- str_split(TracerName, ";")[[1]]
+  ModeOfAdministration <- str_split(ModeOfAdministration, ";")[[1]]
+  InstitutionName <- str_split(InstitutionName, ";")[[1]]
+  PharmaceuticalName <- str_split(PharmaceuticalName, ";")[[1]]
 
   all_tibble <- tibble::as_tibble(
     expand.grid(sub = sub,
