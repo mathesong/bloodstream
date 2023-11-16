da7ae0422000b31a3c58dfb205ce919ea43610ea
mathesong <mathesong@gmail.com>
2023-11-17 00:19:19 +0100

    update the R script to accept arguments directly, and allow the use of no config file at all

--- docker/run_bloodstream.R
+++ docker/run_bloodstream.R
@@ -1,9 +1,23 @@
-# run_bloodstream.R
+#!/usr/bin/env -S Rscript --vanilla
+args <- commandArgs(trailingOnly = TRUE)
+
+# Sort out config file
+config_file <- NA
+
+if (length(args) > 0){
+  config_file <- paste0("code/bloodstream/", args[1])
+}
+
+# And BIDS directory
 bids_dir <- file.path('/data')
-config_file <- file.path(bids_dir, 'code/bloodstream', 'config.json')
 
 # Load necessary libraries
 library(bloodstream)
 
 # Execute bloodstream
-bloodstream(bids_dir, config_file)
+if( !is.na(config_file) ) {
+  bloodstream(bids_dir, config_file)
+} else {
+  bloodstream(bids_dir)
+}
+
