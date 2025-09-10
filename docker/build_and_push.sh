b6983477816df6db5dddb5bb0d229232437f52e8
Granville Matheson <mathesong@gmail.com>
2025-09-10 16:07:18 +0200

    Fixed echo stuff in the template qmd

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -1,3 +1,7 @@
-docker build -t mathesong/bloodstream:latest . --platform linux/amd64
+#!/bin/bash
+
+# Build Docker image from the root directory using the dockerfile in docker subdirectory
+cd ..
+docker build -f docker/dockerfile -t mathesong/bloodstream:latest . --platform linux/amd64
 docker push mathesong/bloodstream:latest
 
