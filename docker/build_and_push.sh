795d98421ebc36ab856de02b0f0a49be0abf908e
Granville Matheson <mathesong@gmail.com>
2025-10-23 18:12:46 +0200

    Update docker build command to allow versions

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-
 # Build Docker image from the root directory using the dockerfile in docker subdirectory
 cd ..
-docker build -f docker/dockerfile -t mathesong/bloodstream:latest . --platform linux/amd64
-docker push mathesong/bloodstream:latest
 
+# Set version
+VERSION="v0.2.0"
+
+# Build with version tag
+docker build -f docker/dockerfile -t mathesong/bloodstream:${VERSION} . --platform linux/amd64
+
+# Tag as latest
+docker tag mathesong/bloodstream:${VERSION} mathesong/bloodstream:latest
+
+# Push both tags
+docker push mathesong/bloodstream:${VERSION}
+docker push mathesong/bloodstream:latest
