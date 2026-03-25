ed6ebef7273fea32f0e5313405625084ed2dfead
Granville Matheson <mathesong@gmail.com>
2026-03-25 12:23:52 +0100

    Bump version number for R API consistency improvements

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -3,7 +3,7 @@
 cd ..
 
 # Set version
-VERSION="v0.2.1"
+VERSION="v0.2.2"
 
 # Build with version tag
 docker build -f docker/dockerfile -t mathesong/bloodstream:${VERSION} . --platform linux/amd64
